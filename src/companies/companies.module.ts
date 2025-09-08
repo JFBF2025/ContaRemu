fdc5e74b159840d5e0065958ecf456f6b80e0767
JFBF2025 <jbecerra81@gmail.com>
2025-09-08 20:34:08 -0300

    Update companies.module.ts

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
-
 @Module({ providers: [CompaniesService], controllers: [CompaniesController] })
 export class CompaniesModule {}
