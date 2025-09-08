9be04040464eb8b2d385d1a334a3ffa7be961825
JFBF2025 <jbecerra81@gmail.com>
2025-09-08 20:35:39 -0300

    Update 1700000000000-init-schema.ts

--- src/database/migrations/1700000000000-init-schema.ts
+++ src/database/migrations/1700000000000-init-schema.ts
@@ -1,7 +1,6 @@
 import { AppDataSource } from '../../../ormconfig';
 import { readFileSync } from 'node:fs';
 import { resolve } from 'node:path';
-
 async function run() {
   const ds = await AppDataSource.initialize();
   const sql = readFileSync(resolve(process.cwd(), 'schema.sql'), 'utf8');
@@ -9,5 +8,4 @@ async function run() {
   await ds.destroy();
   console.log('Schema created');
 }
-
 if (process.argv.includes('--run')) run();
