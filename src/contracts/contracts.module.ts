a4ba30fddce39961a0f3421158b9b82ac60da577
JFBF2025 <jbecerra81@gmail.com>
2025-09-08 20:35:09 -0300

    Update contracts.module.ts

--- src/contracts/contracts.module.ts
+++ src/contracts/contracts.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
 import { ContractsController } from './contracts.controller';
-
 @Module({ controllers: [ContractsController] })
 export class ContractsModule {}
