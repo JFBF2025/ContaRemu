a63c44b344faf58660497bc3b8f61641c7143344
JFBF2025 <jbecerra81@gmail.com>
2025-09-08 20:36:42 -0300

    Update employees.module.ts

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
 import { EmployeesController } from './employees.controller';
-
 @Module({ controllers: [EmployeesController] })
 export class EmployeesModule {}
